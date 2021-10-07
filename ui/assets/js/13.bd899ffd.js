238f1aa1b27f9fedeb6dccf46deda4c8b66126e6
brainpoint <bpoint.lee@gmail.com>
2021-10-07 14:09:16 +0800

    update

--- ui/assets/js/13.bd899ffd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{522:function(n,o,w){"use strict";window.$BpGlobalWithoutInjectNavbarObject=!0}}]);
---
+++ ui/assets/js/15.cd3a1008.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{526:function(n,o,w){"use strict";window.$BpGlobalWithoutInjectNavbarObject=!0}}]);
